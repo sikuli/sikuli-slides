346daf4f362fde162ac29ade6824d3b538a30762
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-03-18 00:09:46 -0600

    add context screenshot class for screen region

---
+++ src/main/java/org/sikuli/slides/screenshots/ContextualScreenshot.java
@@ -0,0 +1,66 @@
+/**
+Khalid
+*/
+package org.sikuli.slides.screenshots;
+
+public class ContextualScreenshot{
+	
+	private String name;
+	private String fileName;
+	private int cx;
+	private int cy;
+	private int offX;
+	private int offY;
+	private int width;
+	private int height;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public int getCx() {
+		return cx;
+	}
+	public void setCx(int cx) {
+		this.cx = cx;
+	}
+	public int getOffX() {
+		return offX;
+	}
+	public void setOffX(int offX) {
+		this.offX = offX;
+	}
+	public int getCy() {
+		return cy;
+	}
+	public void setCy(int cy) {
+		this.cy = cy;
+	}
+	public int getOffY() {
+		return offY;
+	}
+	public void setOffY(int offY) {
+		this.offY = offY;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+}
