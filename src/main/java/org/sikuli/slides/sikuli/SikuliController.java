51f6295107e1f4b7a7374b892cce31510da3a909
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-04-04 22:22:48 -0600

    add external monitor support

--- src/main/java/org/sikuli/slides/sikuli/SikuliController.java
+++ src/main/java/org/sikuli/slides/sikuli/SikuliController.java
@@ -3,10 +3,11 @@ Khalid
 */
 package org.sikuli.slides.sikuli;
 
+import org.sikuli.api.DesktopScreenRegion;
 import org.sikuli.api.ScreenRegion;
+import org.sikuli.api.robot.desktop.DesktopScreen;
 import org.sikuli.api.visual.Canvas;
 import org.sikuli.api.visual.DesktopCanvas;
-
 import org.sikuli.slides.utils.Constants;
 
 public class SikuliController {
@@ -20,5 +21,11 @@ public class SikuliController {
 		// Display the canvas around the target
 		canvas.addBox(screenRegion);
 		canvas.display(Constants.CANVAS_DURATION);
+	}
+	
+	public static ScreenRegion getFullScreenRegion(){
+		ScreenRegion fullScreenRegion=new DesktopScreenRegion();
+		fullScreenRegion.setScreen(new DesktopScreen(Constants.ScreenId));
+		return fullScreenRegion;
 	}
 }
