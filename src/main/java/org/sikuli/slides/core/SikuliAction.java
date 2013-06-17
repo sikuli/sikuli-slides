1c5fb128d1578f228434b4df59635dc416c3d83a
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-06-17 00:02:49 -0600

    change inner class SikuliAction into outer class

---
+++ src/main/java/org/sikuli/slides/core/SikuliAction.java
@@ -0,0 +1,27 @@
+/**
+Khalid
+*/
+package org.sikuli.slides.core;
+
+import java.io.File;
+import org.sikuli.slides.media.Sound;
+import org.sikuli.slides.screenshots.Screenshot;
+import org.sikuli.slides.screenshots.SlideTargetRegion;
+import org.sikuli.slides.shapes.SlideShape;
+import org.sikuli.slides.utils.Constants.DesktopEvent;
+
+public class SikuliAction{
+	private SlideComponent slideComponent;
+	private SlideShape slideShape;
+	private DesktopEvent desktopEvent;
+	public SikuliAction(File imageTargetFile, SlideShape slideShape, Screenshot screenshot, 
+			SlideTargetRegion slideTargetRegion,DesktopEvent desktopEvent, Sound sound, SlideShape label){
+		this.slideShape=slideShape;
+		this.desktopEvent=desktopEvent;
+		slideComponent=new SlideComponent(imageTargetFile, 
+				slideShape, screenshot, slideTargetRegion, sound, label);
+	}
+	public void doSikuliAction(){
+		slideShape.doSikuliAction(slideComponent,desktopEvent);
+	}
+}
