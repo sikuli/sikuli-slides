27b1a089f94b8c395ee2a02837e19f33286157c2
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-07-06 10:08:27 -0600

    add new menu bar

--- src/main/java/org/sikuli/slides/guis/MainUI.java
+++ src/main/java/org/sikuli/slides/guis/MainUI.java
@@ -10,7 +10,6 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,20 +17,24 @@ import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.text.DefaultCaret;
-
 import org.sikuli.slides.Main;
 import org.sikuli.slides.utils.Constants;
 import org.sikuli.slides.utils.MyFileFilter;
+import org.sikuli.slides.utils.Utils;
 import org.sikuli.slides.utils.logging.TextAreaAppender;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Logger;
@@ -40,7 +43,7 @@ import ch.qos.logback.classic.PatternLayout;
 
 
 
-public class MainUI extends JFrame implements ActionListener, ChangeListener, KeyListener {
+public class MainUI extends JFrame implements ActionListener, ChangeListener {
 	private static final long serialVersionUID = -839784598366545263L;
     private JButton btn_open;
     private JComboBox runningModeList;
@@ -48,7 +51,9 @@ public class MainUI extends JFrame implements ActionListener, ChangeListener, Ke
     private JLabel statusLabel;
     public JTextArea logArea;
     private JPanel bottomPanel;
-    
+    private JMenuBar menuBar;
+    private JMenu fileMenu, editMenu, helpMenu;
+    private JMenuItem openFileMenuItem, quitMenuItem, editPrefsMenuItem, helpMenuItem;
     
 
     public MainUI(){
@@ -56,6 +61,52 @@ public class MainUI extends JFrame implements ActionListener, ChangeListener, Ke
     }
     
     private void createAndShowUI() {
+    	// create the menu
+    	menuBar = new JMenuBar();
+    	
+    	fileMenu = new JMenu("File");
+    	fileMenu.setMnemonic(KeyEvent.VK_F); // Use keyboard shortcut: Alt + F
+    	fileMenu.getAccessibleContext().setAccessibleDescription("File menu that has the main menu items.");
+    	menuBar.add(fileMenu);
+    	
+    	openFileMenuItem = new JMenuItem("Open File...");
+    	openFileMenuItem.setAccelerator(KeyStroke.getKeyStroke(
+    	        KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+    	openFileMenuItem.getAccessibleContext().setAccessibleDescription("Open File");
+    	openFileMenuItem.addActionListener(this);
+    	fileMenu.add(openFileMenuItem);
+    	
+    	quitMenuItem = new JMenuItem("Quit Sikuli-Slides");
+    	quitMenuItem.setAccelerator(KeyStroke.getKeyStroke(
+    	        KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+    	quitMenuItem.getAccessibleContext().setAccessibleDescription("Quit Sikuli-Slides");
+    	quitMenuItem.addActionListener(this);
+    	fileMenu.add(quitMenuItem);
+    	
+    	editMenu = new JMenu("Edit");
+    	editMenu.setMnemonic(KeyEvent.VK_E); // Use keyboard shortcut: Alt + E
+    	editMenu.getAccessibleContext().setAccessibleDescription("Edit menu that has the user's preferences and settings");
+    	menuBar.add(editMenu);
+    	
+    	editPrefsMenuItem = new JMenuItem("Edit preferences...");
+    	editPrefsMenuItem.setAccelerator(KeyStroke.getKeyStroke(
+    	        KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+    	editPrefsMenuItem.getAccessibleContext().setAccessibleDescription("Edit preferences");
+    	editPrefsMenuItem.addActionListener(this);
+    	editMenu.add(editPrefsMenuItem);
+    	
+    	helpMenu = new JMenu("Help");
+    	helpMenu.setMnemonic(KeyEvent.VK_H); // Use keyboard shortcut: Alt + H
+    	helpMenu.getAccessibleContext().setAccessibleDescription("Help menu");
+    	menuBar.add(helpMenu);
+    	
+    	helpMenuItem = new JMenuItem("Sikuli-Slides Help");
+    	helpMenuItem.getAccessibleContext().setAccessibleDescription("SikuliSlides Help");
+    	helpMenuItem.addActionListener(this);
+    	helpMenu.add(helpMenuItem);
+    	
+    	setJMenuBar(menuBar);
+    	
     	JToolBar horizontal_toolbar=new JToolBar();
         horizontal_toolbar.setFloatable(false); // to make a tool bar immovable
         horizontal_toolbar.setMargin(new Insets(2, 5, 5, 5));
@@ -116,7 +167,6 @@ public class MainUI extends JFrame implements ActionListener, ChangeListener, Ke
         
         add(bottomPanel, BorderLayout.SOUTH);
 
-        addKeyListener(this);
         setFocusable(true);
         
         setTitle("Sikuli-Slides");
@@ -126,39 +176,16 @@ public class MainUI extends JFrame implements ActionListener, ChangeListener, Ke
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-    
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if ((e.getKeyCode() == KeyEvent.VK_O) && 
-				((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
-            // open the .pptx file
-            File file=openFile();
-            runSikuli(file);
-		}
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		  // open .pptx file
-        if (e.getSource() == btn_open) {
+        if (e.getSource() == btn_open || e.getSource() == openFileMenuItem) {
                 // open the .pptx file
                 File file=openFile();
                 runSikuli(file);
         }
-        else if(e.getSource()==runningModeList){
+        else if(e.getSource() == runningModeList){
         	if(runningModeList.getSelectedIndex()==0){
         		Constants.ACTION_MODE=true;
         		Constants.HELP_MODE=false;
@@ -174,6 +201,16 @@ public class MainUI extends JFrame implements ActionListener, ChangeListener, Ke
         		Constants.HELP_MODE=false;
         		Constants.TUTORIAL_MODE=true;
         	}
+        }
+        else if(e.getSource() == editPrefsMenuItem){
+        	// open preferences editor UI
+        }
+        else if(e.getSource() == helpMenuItem){
+        	// open sikuli-slides help
+        	Utils.openURLInBrowser("http://sikuli.org");
+        }
+        else if(e.getSource() == quitMenuItem){
+        	System.exit(0);
         }
 	}
 	
