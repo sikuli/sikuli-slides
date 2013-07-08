c4d310d7f6183b19ff3c1e4aa497b99509adb599
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-07-07 22:53:24 -0600

    add get screen bounds to utils

--- src/main/java/org/sikuli/slides/utils/MyScreen.java
+++ src/main/java/org/sikuli/slides/utils/MyScreen.java
@@ -4,11 +4,20 @@ Khalid
 package org.sikuli.slides.utils;
 
 import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 
 public class MyScreen {
 	
 	public static Dimension getScreenDimensions(){
 		return Toolkit.getDefaultToolkit().getScreenSize();
+	}
+	
+	public static Rectangle getScreenBounds(){
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+		return defaultScreen.getDefaultConfiguration().getBounds();
 	}
 }
