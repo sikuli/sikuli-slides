3ceb2778bfb8f90f16efd116e7a9d035615b6a75
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-07-18 00:15:04 -0400

    add set default connected screen

--- src/main/java/org/sikuli/slides/Main.java
+++ src/main/java/org/sikuli/slides/Main.java
@@ -2,9 +2,12 @@ package org.sikuli.slides;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+import org.sikuli.api.robot.desktop.DesktopScreen;
 import org.sikuli.slides.uis.MainCommandLine;
 import org.sikuli.slides.uis.MainUI;
 import org.sikuli.slides.utils.Constants;
+import org.sikuli.slides.utils.UserPreferencesEditor;
 import org.sikuli.slides.utils.Utils;
 import org.sikuli.slides.core.SikuliPowerPoint;
 import org.slf4j.Logger;
@@ -22,13 +25,27 @@ import org.slf4j.LoggerFactory;
 
 public class Main{
 	private static final Logger logger = (Logger) LoggerFactory.getLogger(Main.class);
-	  
+	private UserPreferencesEditor prefsEditor = new UserPreferencesEditor();
+	
 	private void initProject(){
 		// create the project working directory directory
 		if(!Utils.createWorkingDirectory()){
 			logger.error("Failed to create sikuli-slides working directory in +"+System.getProperty("java.io.tmpdir"));
 			System.exit(1);
 		}
+		// check default screen
+		checkScreen();
+	}
+	// check if the value of the screen id is connected.
+	private void checkScreen() {
+		int numberOfScreens = DesktopScreen.getNumberScreens();
+		int storedScreenId = prefsEditor.getDisplayId();
+		// if the display is disconnected, update the screen id to the default screen
+		if(storedScreenId >= numberOfScreens){
+			storedScreenId = 0;
+			prefsEditor.putDisplayId(storedScreenId);
+		}
+		Constants.ScreenId = storedScreenId;
 	}
 
 	/**
